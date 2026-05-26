6faa617292f6f28d815874b66afff0b54361f9f8
josiyevenes <josianne.yevenes@gmail.com>
2026-05-26 18:27:11 -0400

    Semana 1 - 1.1
    Crear estructura del proyecto:
    - Entorno Virtual
    - Instalar dependencias
    - Crear proyecto
    - Crear 4 apps del sistema

--- byfamco/settings.py
+++ byfamco/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'accounts',
+    'familias',
+    'freelancers',
+    'finanzas',
+    'documentos',
 ]
 
 MIDDLEWARE = [
